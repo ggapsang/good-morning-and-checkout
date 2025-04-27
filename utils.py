90297821683441d52cbbb5e80c690d13fb4840d9
DESKTOP-HVH1CSC\ASUS <ggapsang@gmail.com>
2025-04-27 23:40:22 +0900

    fix : caching youtube search link

--- utils.py
+++ utils.py
@@ -34,33 +34,14 @@ def render_today_market(df, container, jpy_100=False):
         st.write(f"<span style='color:{color};'>{change_percent}%</span>", unsafe_allow_html=True)
 
 
-def get_latest_video_by_keyword(youtube, channel, search_query, is_handle=True):
-    """
-    채널 이름 또는 ID로 최신 동영상 검색
-    is_handle이 True면 채널 핸들(@없는 형태)로 간주, False면 채널 ID로 간주
-    """
-    if is_handle:
-        # 핸들이라면 @ 제거
-        if channel.startswith('@'):
-            channel = channel[1:]
-            
-        # 먼저 핸들로 채널 ID 찾기
-        channel_request = youtube.search().list(
-            part="snippet",
-            q=channel,
-            type="channel",
-            maxResults=1
-        )
-        channel_response = channel_request.execute()
-        
-        if not channel_response['items']:
-            return None
-            
-        channel_id = channel_response['items'][0]['id']['channelId']
-    else:
-        channel_id = channel
+@st.cache_data(ttl=3600*12)  # 12시간 동안 캐싱
+def get_latest_video_by_keyword(youtube, channel, search_query, is_handle=False):
+    """채널 ID로 최신 동영상 검색 (캐싱 적용)"""
     
-    # 이제 찾은 채널 ID로 검색
+    # is_handle 파라미터가 False일 때는 channel을 ID로 바로 사용
+    channel_id = channel
+    
+    # 채널 ID로 바로 검색
     request = youtube.search().list(
         part="snippet",
         channelId=channel_id,
@@ -70,16 +51,23 @@ def get_latest_video_by_keyword(youtube, channel, search_query, is_handle=True):
         order="date"
     )
     
-    response = request.execute()
-    
-    item = response['items'][0]
-    video_id = item['id']['videoId']
-    title = item['snippet']['title']
-    url = f"https://www.youtube.com/watch?v={video_id}"
-    published_at = item['snippet']['publishedAt']
-    
-    return {
-        'title': title,
-        'url': url,
-        'published_at': published_at
-    }
+    try:
+        response = request.execute()
+        
+        if response.get('items') and len(response['items']) > 0:
+            item = response['items'][0]
+            video_id = item['id']['videoId']
+            title = item['snippet']['title']
+            url = f"https://www.youtube.com/watch?v={video_id}"
+            published_at = item['snippet']['publishedAt']
+            
+            return {
+                'title': title,
+                'url': url,
+                'published_at': published_at
+            }
+        else:
+            return None
+    except Exception as e:
+        st.error(f"API 오류: {str(e)}")
+        return None
